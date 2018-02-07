9f74a0fafc2aaa7b33fef941ba1cce9ddc5b7b4f
Jordan Martinez <jordanalex.martinez@gmail.com>
2018-02-06 16:41:43 -0800

    Removed deprecated inhibeans package

--- reactfx/src/main/java/org/reactfx/collection/SuspendableList.java
+++ reactfx/src/main/java/org/reactfx/collection/SuspendableList.java
@@ -7,9 +7,6 @@ import org.reactfx.Suspendable;
  * An observable list whose items are the same as its {@code sourceList} when unsuspended; once suspended,
  * any changes to the {@code sourceList} will not propagate to this list until this list is unsuspended.
  */
-@SuppressWarnings("deprecation")
 public interface SuspendableList<E>
-extends LiveList<E>, Suspendable, org.reactfx.inhibeans.collection.ObservableList<E> {
-    @Override @Deprecated
-    default Guard block() { return suspend(); }
+extends LiveList<E>, Suspendable {
 }
